a81a00f4d75274340fe44d677a89a30d257e2f46
Vokymir <22869857+vokymir@users.noreply.github.com>
2025-12-09 18:46:43 +0100

    session WIP

--- semestralka/server/include/prsi/net/session.hpp
+++ semestralka/server/include/prsi/net/session.hpp
@@ -1,9 +1,21 @@
 #pragma once
 
+#include <chrono>
+#include <string>
+
 namespace prsi::net {
 
 // Handle message recv/send buffers, pings.
 // Use protocol.
-class Session {};
+class Session {
+  int fd_;
+  std::string nick_;
+
+  std::string read_buffer_;
+  std::string write_buffer_;
+
+  std::chrono::steady_clock::time_point last_recv_;
+  std::chrono::steady_clock::time_point last_send_;
+};
 
 } // namespace prsi::net
