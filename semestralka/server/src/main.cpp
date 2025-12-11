7dd6b754625307bb70150202a618f01b995f89a5
Vokymir <22869857+vokymir@users.noreply.github.com>
2025-12-11 15:41:53 +0100

    dummy main

--- semestralka/server/src/main.cpp
+++ semestralka/server/src/main.cpp
@@ -1,7 +1,15 @@
 
+#include "config.hpp"
 #include "logger.hpp"
+#include "server.hpp"
 
 int main(int argc, char **argv) {
   prsi::Logger::info("Server started.");
+
+  prsi::Config cfg;
+  prsi::Server s{cfg};
+
+  s.run();
+
   return 0;
 }
