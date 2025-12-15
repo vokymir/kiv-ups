2ab22b7eb6d249cba24613987d0bc2e1a0793631
Vokymir <22869857+vokymir@users.noreply.github.com>
2025-12-15 09:42:23 +0100

    client WIP net -> ui

--- semestralka/client/prsi/client.py
+++ semestralka/client/prsi/client.py
@@ -1,7 +1,7 @@
 from queue import Queue
 from tkinter import messagebox
 import queue
-from prsi.net import Net, Queue_Message
+from prsi.net import QM_DISCONNECTED, QM_ERROR, QM_MESSAGE, Net, Queue_Message
 from prsi.ui import Ui
 
 
@@ -24,5 +24,32 @@ class Client:
     def run(self) -> None:
         self.ui.mainloop()
 
+    # net -> ui
+
     def process_incoming_messages(self) -> None:
-        pass
+        """
+        Periodically check the queue for messages from the net.
+        This method runs on the main thread = safe to update tkinter.
+        """
+        while (not self.mq.empty()):
+            qm_type, content = self.mq.get()
+
+            if (qm_type == QM_ERROR):
+                _ = messagebox.showerror("Network Error", content if content else "Unknown Error")
+                self.ui.switch_frame("login")
+
+            elif (qm_type == QM_DISCONNECTED):
+                self.ui.switch_frame("login")
+
+            elif (qm_type == QM_MESSAGE):
+                if (content):
+                    self.handle_protocol(content)
+
+        _ = self.ui.after(100, self.process_incoming_messages)
+
+    def handle_protocol(self, msg: str) -> None:
+        print(f"[PROTO] Received: {msg}")
+        parts: list[str] = msg.split()
+        if (not parts):
+            return
+
