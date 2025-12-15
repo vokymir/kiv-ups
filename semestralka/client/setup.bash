86552d5f35f8bc76261825e559d1c00a5983f65c
Vokymir <22869857+vokymir@users.noreply.github.com>
2025-12-15 18:02:56 +0100

    update setup

--- semestralka/client/setup.bash
+++ semestralka/client/setup.bash
@@ -1,8 +1,12 @@
-# manjaro
-pip install typing_extensions
 
-# debian
+# == venv
 sudo apt install python3-venv
 python3 -m venv .venv
 source .venv/bin/activate
+
+# == PIL
+pip install pillow
+
+# == typing
+
 pip install typing_extensions
