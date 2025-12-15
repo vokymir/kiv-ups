6a9930b38e987f29c9fad9e2e6cebc840f0e4885
Vokymir <22869857+vokymir@users.noreply.github.com>
2025-12-15 10:01:18 +0100

    client handle incoming protocol messages PREPARED

--- semestralka/client/prsi/client.py
+++ semestralka/client/prsi/client.py
@@ -3,6 +3,7 @@ from tkinter import messagebox
 import queue
 from prsi.net import QM_DISCONNECTED, QM_ERROR, QM_MESSAGE, Net, Queue_Message
 from prsi.ui import Ui
+from prsi.config import CMD_PONG
 
 
 class Client:
@@ -48,8 +49,80 @@ class Client:
         _ = self.ui.after(100, self.process_incoming_messages)
 
     def handle_protocol(self, msg: str) -> None:
+        """
+        Authoritative place to handle all incoming messages from net.
+        """
         print(f"[PROTO] Received: {msg}")
         parts: list[str] = msg.split()
         if (not parts):
             return
+
+        cmd: str = parts[0]
+
+        match cmd:
+            case "OK":
+                return
+            case "ROOMS":
+                # update lobby
+                pass
+            case "ROOM":
+                # update room
+                pass
+            case "GAME_START":
+                # maybe nothing?
+                pass
+            case "HAND":
+                # update hand
+                pass
+            case "TURN":
+                # update current turn, top card
+                pass
+            case "PLAYED":
+                # show what other player did
+                pass
+            case "DRAWED":
+                # show that other player drew
+                pass
+            case "SKIP":
+                # maybe show sho is skipped
+                pass
+            case "CARDS":
+                # add these cards to hand
+                pass
+            case "WIN":
+                # show you are winner
+                pass
+            case "LOSE":
+                # show you are loser
+                pass
+            case "PING":
+                self.net.send_command(CMD_PONG)
+            case "STATE":
+                # change UI accordingly
+                pass
+            case "SLEEP":
+                # show who is sleeping
+                pass
+            case "DEAD":
+                # show who is dead
+                pass
+            case "AWAKE":
+                # show who is awake
+                pass
+            case _:
+                _ = messagebox.showerror("Unknown message received:",msg)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
