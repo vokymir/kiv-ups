7fc302f92531e56deb29b1f1e185cb356d7016f8
Vokymir <22869857+vokymir@users.noreply.github.com>
2025-12-15 08:11:43 +0100

    ui init starting point

--- semestralka/client/prsi/ui.py
+++ semestralka/client/prsi/ui.py
@@ -1,6 +1,16 @@
+import tkinter as tk
+
+from prsi import config
+
 
 class Client: pass # forward declare
 
-class Ui:
+class Ui(tk.Tk):
     def __init__(self, client: Client) -> None:
-        pass
+        super().__init__()
+        self.client: Client = client
+
+        self.title(config.APP_TITLE)
+        self.geometry(f"{config.WINDOW_WIDTH}x{config.WINDOW_HEIGHT}")
+        _ =self.configure(bg=config.BG_COLOR)
+
