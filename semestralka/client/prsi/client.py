13984e156c25fcfa9049e9ac1ea21175d553a5d0
Vokymir <22869857+vokymir@users.noreply.github.com>
2025-12-16 14:39:39 +0100

    fix client reconnectiong when not in server

--- semestralka/client/prsi/client.py
+++ semestralka/client/prsi/client.py
@@ -67,6 +67,8 @@ class Client(Client_Dummy):
 
     # reconnect stuff
     def check_server_availability(self) -> None:
+        if (self.player and self.player.state != ST_UNNAMED):
+            return
         now: datetime = datetime.now(timezone.utc)
         elapsed: timedelta = now - self.last_ping_recv
         print(f"ELAPSED {elapsed.seconds}")
